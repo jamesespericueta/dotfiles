56a3d8a399c0cd8106552a1e7120008fe53d450a
james <james@pop-os.localdomain>
2020-10-13 18:34:30 -0400

    the sexiest part

---
+++ i3-radius/build.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# compile & install
+autoreconf --force --install
+rm -rf build/
+mkdir -p build && cd build/
+
+# Disabling sanitizers is important for release versions!
+# The prefix and sysconfdir are, obviously, dependent on the distribution.
+
+../configure --prefix=/usr --sysconfdir=/etc --disable-sanitizers
+make
+sudo make install
